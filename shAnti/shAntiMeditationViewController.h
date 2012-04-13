b69854c77a42a817a6f5dc3e90bb48c1d5c6254d
NuttySimple <MacbookPro@Jordans-MacBook-Pro.local>
2012-04-13 15:56:31 -0700

    Commit

---
+++ shAnti/shAntiMeditationViewController.h
@@ -0,0 +1,13 @@
+//
+//  shAntiMeditationViewController.h
+//  shAnti
+//
+//  Created by Jordan Gurrieri on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface shAntiMeditationViewController : UIViewController
+
+@end
---
+++ shAnti/shAntiMeditationViewController.m
@@ -0,0 +1,43 @@
+//
+//  shAntiMeditationViewController.m
+//  shAnti
+//
+//  Created by Jordan Gurrieri on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "shAntiMeditationViewController.h"
+
+@interface shAntiMeditationViewController ()
+
+@end
+
+@implementation shAntiMeditationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
